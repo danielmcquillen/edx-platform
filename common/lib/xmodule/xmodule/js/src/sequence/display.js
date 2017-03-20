9863edc0aea0c528234b413cb5d329484a142d0d
danielmcquillen <daniel@mcquilleninteractive.com>
2017-03-20 15:10:56 +1100

    add missing "ibio-"

--- common/lib/xmodule/xmodule/js/src/sequence/display.js
+++ common/lib/xmodule/xmodule/js/src/sequence/display.js
@@ -318,7 +318,7 @@
         Sequence.prototype.link_for = function(position) {
             //iBio: Changing this to grab from left nav, not top nav
             //return this.$('#sequence-list .nav-item[data-element=' + position + ']');
-            return $('#ibio-sequence-list .nav-item[data-element=' + position + ']');
+            return $('#ibio-sequence-list .ibio-nav-item[data-element=' + position + ']');
         };
 
         Sequence.prototype.mark_visited = function(position) {

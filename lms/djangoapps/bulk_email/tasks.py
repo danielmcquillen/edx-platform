f1dbee015aa97a1dab02014ae62c5310ddd65529
Daniel McQuillen <daniel@mcquilleninteractive.com>
2019-02-07 12:45:59 +1100

    Abominable hack to prevent Open edX from prefixing reply email with course title

--- lms/djangoapps/bulk_email/tasks.py
+++ lms/djangoapps/bulk_email/tasks.py
@@ -477,8 +477,12 @@ def _send_course_email(entry_id, email_id, to_list, global_email_context, subtas
     course_title = global_email_context['course_title']
 
     # use the email from address in the CourseEmail, if it is present, otherwise compute it
-    from_addr = course_email.from_addr if course_email.from_addr else \
-        _get_source_address(course_email.course_id, course_title)
+    # iBio :
+    # HACK!! This function shouldn't be called since we've defined default from addresses.
+    # But somehow it still is when we do a bulk email, giving a from address of LE-courses@ibiology.org
+    #from_addr = course_email.from_addr if course_email.from_addr else \
+    #    _get_source_address(course_email.course_id, course_title)
+    from_addr = "courses@ibiology.org"
 
     # use the CourseEmailTemplate that was associated with the CourseEmail
     course_email_template = course_email.get_template()

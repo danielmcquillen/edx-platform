0356b257b1e1e0122b2d53711057a3ad9f22abb8
Daniel McQuillen <daniel@mcquilleninteractive.com>
2019-02-07 13:17:25 +1100

    Fix bug. Make unicode string for spanish name.

--- lms/djangoapps/static_template_view/views.py
+++ lms/djangoapps/static_template_view/views.py
@@ -255,7 +255,7 @@ ibio_speaker_arr = [
         "course": "LE"
     },
     {
-        "name": "Daniel Colon-Ramos",
+        "name": u"Daniel Colón-Ramos",
         "videoURL": "https://www.youtube.com/embed/5WVUOsnr8jA",
         "speaker_slug": "daniel-colon-ramos",
         "suffix": "Ph.D.",
